5a81bdd89302a5fd73ad3a4437f96c9540dabb33
kaga <kaga21@gmail.com>
2021-02-02 01:39:51 +1300

    Site is updating, revert change

--- src/components/vehicle-selector/items/VehicleModel.tsx
+++ src/components/vehicle-selector/items/VehicleModel.tsx
@@ -73,7 +73,7 @@ const ModelSelector = GraphqlVehicleSelectorItem<VehicleModelOption, VehicleMode
       return {
         data: data.map((item) => ({
           type: 'MODEL',
-          optionLabel: `${item.name}-${item.id}`,
+          optionLabel: `${item.name}`,
           ...item,
         })),
       };
